9116f41dbcc199bf9f62f4873aa6f511cb140b4a
Matheus-Faneco <matheusfaneco2009@hotmail.com>
2025-05-14 13:45:21 -0300

    Adiciona corsheaders

--- expenseTracker/expenseTracker/settings.py
+++ expenseTracker/expenseTracker/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'expenses',
     'user'
 ]
@@ -50,6 +51,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'user.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +136,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
